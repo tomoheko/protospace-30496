77b70f7b86a39617ba7cad3d628c176be05d8c09
tomoheko <dekirumadeyamenai@gmail.com>
2020-10-13 19:12:36 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to: 'prototypes#index'
+  devise_for :users
+  resources :users, only: [:show, :edit]
+  resources :prototypes do
+    resources :comments, only: [:create]
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
